d20467102f32e348d6e5868a461ae5754ca6892a
Mehul2112 <72200573+Mehul2112@users.noreply.github.com>
2021-05-08 11:24:09 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+
+import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
+import WelcomeScreen from "./app/screens/WelcomeScreen";
+
+export default function App() {
+  return (
+    <NavigationContainer theme={navigationTheme}>
+      <AppNavigator />
+    </NavigationContainer>
+  );
+}
